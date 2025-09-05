59c0f3b5a66ab69d1f00bfbe120b1b15b2ec1b9c
kalu5 <451660550@qq.com>
2025-09-05 22:03:37 +0800

    fix(useResizeObserver): cleaning directives when components are unmounted(vueuse#5001)

--- packages/core/useResizeObserver/directive.test.ts
+++ packages/core/useResizeObserver/directive.test.ts
@@ -2,7 +2,7 @@ import type { VueWrapper } from '@vue/test-utils'
 import type { UseResizeObserverOptions } from './index'
 import { mount } from '@vue/test-utils'
 import { beforeEach, describe, expect, it, vi } from 'vitest'
-import { defineComponent } from 'vue'
+import { defineComponent, nextTick } from 'vue'
 import { vResizeObserver } from './directive'
 
 const App = defineComponent({
@@ -69,6 +69,57 @@ describe('vResizeObserver', () => {
 
     it('should be defined', () => {
       expect(wrapper).toBeDefined()
+    })
+  })
+
+  describe('cleaning directives when components are unmounted', () => {
+    // mock ResizeObserver
+    globalThis.ResizeObserver = vi.fn().mockImplementation((callback) => {
+      return {
+        observe: vi.fn((target) => {
+          callback([{ contentRect: { width: target.offsetWidth || 0 } }], this)
+        }),
+        unobserve: vi.fn(),
+        disconnect: vi.fn(),
+      }
+    })
+    beforeEach(() => {
+      onResizeObserver = vi.fn()
+
+      wrapper = mount(App, {
+        props: {
+          onResizeObserver,
+        },
+        global: {
+          directives: {
+            'resize-observer': vResizeObserver,
+          },
+        },
+      })
+    })
+
+    const observer = new ResizeObserver(onResizeObserver)
+
+    it('should clean up the directives', async () => {
+      await nextTick()
+      const element = wrapper.element.querySelector('div')
+      if (element) {
+        observer.observe(element)
+
+        element.style.width = '200px'
+
+        await new Promise(resolve => setTimeout(resolve, 100))
+
+        expect(onResizeObserver).toBeCalledTimes(1)
+
+        observer.disconnect()
+        wrapper.unmount()
+        onResizeObserver.mockClear()
+        await nextTick()
+        element.style.width = '300px'
+        await new Promise(resolve => setTimeout(resolve, 100))
+        expect(onResizeObserver).toBeCalledTimes(0)
+      }
     })
   })
 })
